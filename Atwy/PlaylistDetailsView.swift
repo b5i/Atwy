3af66e99061868585b8160e061da39e0c280b5a3
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2024-03-14 16:16:01 +0100

    Better UI for PlaylistDetailsView using another API.

--- Atwy/PlaylistDetailsView.swift
+++ Atwy/PlaylistDetailsView.swift
@@ -44,7 +44,48 @@ struct PlaylistDetailsView: View {
                             )
                         }
                     }
-                    .padding(.top, topPaddingForInformations)
+                    .customHeaderView({
+                        /*
+                        ZStack {
+                            Rectangle()
+                                .foregroundStyle(.clear)
+                                .frame(width: geometry.size.width, height: (playlist.channel?.name != nil ? 30 : 0) + ((model.playlistInfos?.viewCount != nil || playlist.timePosted != nil || model.playlistInfos?.videoCount != nil) ? 30 : 0))
+                                .background(.ultraThickMaterial)*/
+                            VStack {
+                                if let channelName = playlist.channel?.name {
+                                    Text(channelName)
+                                        .font(.title3)
+                                        .frame(height: 30)
+                                }
+                                if model.playlistInfos?.viewCount != nil || playlist.timePosted != nil || model.playlistInfos?.videoCount != nil {
+                                    HStack {
+                                        Text(model.playlistInfos?.videoCount ?? "")
+                                            .foregroundColor(colorScheme.textColor)
+                                            .font(.footnote)
+                                            .opacity(0.5)
+                                        if (model.playlistInfos?.viewCount != nil || playlist.timePosted != nil) && model.playlistInfos?.videoCount != nil {
+                                            Divider()
+                                        }
+                                        Text(model.playlistInfos?.viewCount ?? "")
+                                            .foregroundColor(colorScheme.textColor)
+                                            .font(.footnote)
+                                            .opacity(0.5)
+                                        if model.playlistInfos?.viewCount != nil, playlist.timePosted != nil {
+                                            Divider()
+                                        }
+                                        Text(playlist.timePosted ?? "")
+                                            .foregroundColor(colorScheme.textColor)
+                                            .font(.footnote)
+                                            .opacity(0.5)
+                                    }
+                                    .frame(height: 20)
+                                }
+                            }
+                            .padding(.horizontal)
+                        //}
+                        //.frame(width: geometry.size.width, height: topPaddingForInformations)
+                    }, height: topPaddingForInformations)
+                    //.padding(.top, topPaddingForInformations)
                 }
                 if VPM.video != nil {
                     Color.clear.frame(width: 0, height: 70)
@@ -56,6 +97,7 @@ struct PlaylistDetailsView: View {
                 }
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
+            /*
             .overlay(alignment: .top, content: {
                 ZStack {
                     Rectangle()
@@ -95,7 +137,7 @@ struct PlaylistDetailsView: View {
                     .padding(.horizontal)
                 }
                 .frame(width: geometry.size.width, height: topPaddingForInformations)
-            })
+            })*/
         }
 #if !os(macOS)
         .navigationBarTitleDisplayMode(.inline)
