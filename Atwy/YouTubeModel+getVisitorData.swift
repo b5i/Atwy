23411a33385b0eea5f4cfd46ae923d3363cff604
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2025-07-19 21:39:01 +0200

    chore: update YouTubeKit to potentially fix issues with the player

--- Atwy/YouTubeModel+getVisitorData.swift
+++ Atwy/YouTubeModel+getVisitorData.swift
@@ -11,9 +11,12 @@ import OSLog
 
 extension YouTubeModel {
     func getVisitorData() async {
+        let oldVisitorData = YTM.visitorData
+        YTM.visitorData = ""
         if let visitorData = try? await SearchResponse.sendThrowingRequest(youtubeModel: YTM, data: [.query: "mrbeast"]).visitorData {
             YTM.visitorData = visitorData
         } else {
+            YTM.visitorData = oldVisitorData
             Logger.atwyLogs.simpleLog("Couldn't get visitorData, request may fail.")
         }
     }
